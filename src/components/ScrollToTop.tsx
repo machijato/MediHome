73c2c504e9f4ee6543e3fea35d116dbf7d4c7f51
machijato <makiblaz@googlemail.com>
2026-06-11 09:27:06 +0200

    Include query changes in scroll reset

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,11 +2,11 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'instant' });
-  }, [pathname]);
+  }, [pathname, search]);
 
   return null;
 }
